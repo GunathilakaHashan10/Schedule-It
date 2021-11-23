ef759a64c195a7862d249edcabf53756adc483ea
Hashan Gunathilaka <hashan5912014@gmail.com>
2021-11-23 22:41:20 +0530

    changed secrets

--- lib/helpers/secrets.dart
+++ lib/helpers/secrets.dart
@@ -1,6 +1,6 @@
 class Secrets {
-  static const String API_KEY = 'AIzaSyDf160ta7qqt6RXMJerVz42lcmGd_w3O_M';
-  static const String FIREBASE_URL = 'https://schedule-it-app-default-rtdb.asia-southeast1.firebasedatabase.app';
+  static const String API_KEY = ''; // Google API Key
+  static const String FIREBASE_URL = ''; // Firebase URL
   static const String FIREBASE_AUTH_URL = 'https://identitytoolkit.googleapis.com';
   static const String FIREBASE_REFRESH_ID_TOKEN_URL = 'https://securetoken.googleapis.com';
 }
